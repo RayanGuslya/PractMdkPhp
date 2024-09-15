44b80615a41dff1c32e812c756a79d703455f575
RayanGuslya <sesmeneev@gmail.com>
2024-09-15 14:34:18 +0300

    add composer

---
+++ BookPhp/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alexander/book-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '27c45275439759d2803343af124688627004a1a5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexander/book-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '27c45275439759d2803343af124688627004a1a5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
