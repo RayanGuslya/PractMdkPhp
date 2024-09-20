d538d1925012569495c50288da6ce3e9ab384728
RayanGuslya <sesmeneev@gmail.com>
2024-09-20 22:27:06 +0300

    update

--- UnitPhpTest3/src/storage/task3.php
+++ UnitPhpTest3/src/storage/task3.php
@@ -1,39 +1,40 @@
 <?php
 namespace Alexander\UnitPhpTest;
-class Task3 {
-public function caesarCipherRussian(string $text, int $key): string
+class Task3
 {
-    $result = '';
+    public function caesarCipherRussian(string $text, int $key): string
+    {
+        $result = '';
 
-    $alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+        $alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
 
-    $key = $key % mb_strlen($alphabet, 'UTF-8');
+        $key = $key % mb_strlen($alphabet, 'UTF-8');
 
-    for ($i = 0; $i < mb_strlen($text, 'UTF-8'); $i++) {
-        $char = mb_substr($text, $i, 1, 'UTF-8');
+        for ($i = 0; $i < mb_strlen($text, 'UTF-8'); $i++) {
+            $char = mb_substr($text, $i, 1, 'UTF-8');
 
-        if (preg_match('/[а-я]/ui', $char)) {
-            $charPosition = mb_stripos($alphabet, $char);
-            $newPosition = ($charPosition + $key) % mb_strlen($alphabet, 'UTF-8');
-            $result .= mb_substr($alphabet, $newPosition, 1, 'UTF-8');
-        } else {
-            $result .= $char;
+            if (preg_match('/[а-я]/ui', $char)) {
+                $charPosition = mb_stripos($alphabet, $char);
+                $newPosition = ($charPosition + $key) % mb_strlen($alphabet, 'UTF-8');
+                $result .= mb_substr($alphabet, $newPosition, 1, 'UTF-8');
+            } else {
+                $result .= $char;
+            }
         }
+
+        return $result;
     }
 
-    return $result;
-}
+    /*
+    // Ввод текста и сдвига с клавиатуры
+    echo "Введите текст на русском: ";
+    $text = readline();
+    echo "Введите сдвиг (целое число): ";
+    $key = intval(readline());
 
-/*
-// Ввод текста и сдвига с клавиатуры
-echo "Введите текст на русском: ";
-$text = readline();
-echo "Введите сдвиг (целое число): ";
-$key = intval(readline());
+    // Шифрование
+    $encryptedText = caesarCipherRussian($text, $key);
 
-// Шифрование
-$encryptedText = caesarCipherRussian($text, $key);
-
-echo "Зашифрованный текст: " . $encryptedText . "\n";
-*/
+    echo "Зашифрованный текст: " . $encryptedText . "\n";
+    */
 }
