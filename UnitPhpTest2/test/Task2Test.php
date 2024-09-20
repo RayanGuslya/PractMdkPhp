6c811ca7bbd3a02d8cf46041620562a9db003315
RayanGuslya <sesmeneev@gmail.com>
2024-09-20 22:36:40 +0300

    add

---
+++ UnitPhpTest2/test/Task2Test.php
@@ -0,0 +1,32 @@
+<?php
+use Alexander\UnitPhpTest2\Task2;
+use \PHPUnit\Framework\TestCase;
+class Task2Test extends TestCase
+{
+    private $taskTest2;
+    protected function setUp(): void
+    {
+        $this->taskTest2 = new Task2();
+    }
+    //Проверяет, что объект является экземпляром определенного класса
+    public function testArrayUniqueInstanceOf(): void
+    {
+        $this->assertInstanceOf(Task2::class, $this->taskTest2);
+    }
+    //Проверяет, что метод правильно удаляет дубликаты из массива целых чисел
+    public function testArrayUniqueWithDuplicates(): void
+    {
+        $input = [1, 2, 2, 3, 4, 4, 5];
+        $expected = [1, 2, 3, 4, 5];
+        $this->assertSame($expected, $this->taskTest2->arrayUnique($input));
+    }
+    //Проверка с булевыми значениями
+    public function testArrayUniqueContainsBooleanValues(): void
+    {
+        $input = [true, false, true, false];
+        $result = $this->taskTest2->arrayUnique($input);
+
+        $this->assertContains(true, $result);
+        $this->assertContains(false, $result);
+    }
+}
